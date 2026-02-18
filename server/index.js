592c7bba7d0b1377a5c3b53cadee024cfd2f6d1c
Ajay Abraham Jacob <ajayabrahamjacob2007@gmail.com>
2026-02-18 16:37:28 +0530

    fix: Add SPA fallback to serve index.html for unknown routes

--- server/index.js
+++ server/index.js
@@ -177,6 +177,15 @@ io.on('connection', (socket) => {
   });
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
